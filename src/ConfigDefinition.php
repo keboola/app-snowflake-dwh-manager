ecb10cb6f69baf7bdb7c52c7c532596bc8f607b8
Tomáš Fejfar <tomas.fejfar@gmail.com>
2018-06-07 15:04:30 +0200

    ConfigDefinition

--- src/ConfigDefinition.php
+++ src/ConfigDefinition.php
@@ -12,16 +12,63 @@ class ConfigDefinition extends BaseConfigDefinition
     protected function getParametersDefinition(): ArrayNodeDefinition
     {
         $parametersNode = parent::getParametersDefinition();
+        $parametersNode->isRequired();
         // @formatter:off
         /** @noinspection NullPointerExceptionInspection */
         $parametersNode
             ->children()
-                ->scalarNode('foo')
-                    ->defaultValue('baz')
+                ->scalarNode('master_host')
+                    ->isRequired()
                 ->end()
+                ->scalarNode('master_user')
+                    ->isRequired()
+                ->end()
+                ->scalarNode('master_password')
+                    ->isRequired()
+                ->end()
+                ->scalarNode('master_database')
+                    ->isRequired()
+                ->end()
+                ->scalarNode('warehouse')
+                    ->isRequired()
+                ->end()
+                ->arrayNode('business_schema')
+                    ->children()
+                        ->scalarNode('schema_name')
+                            ->isRequired()
+                        ->end()
+                    ->end()
+                ->end()
+                ->arrayNode('user')
+                    ->children()
+                        ->scalarNode('email')
+                            ->isRequired()
+                        ->end()
+                        ->arrayNode('business_schemes')
+                            ->scalarPrototype()
+                        ->end()
+                        ->end()
+                        ->booleanNode('disabled')
+                            ->defaultFalse()
+                        ->end()
+                    ->end()
+                ->end()
+            ->end()
+            ->validate()
+                ->ifTrue(function ($v) {
+                    return isset($v['user']) && isset($v['business_schema']);
+                })
+                ->thenInvalid('The "user" and "business_schema" options are mutually exclusive.')
+            ->end()
+            ->validate()
+                ->ifTrue(function ($v) {
+                    return !isset($v['user']) && !isset($v['business_schema']);
+                })
+                ->thenInvalid('Either "user" or "business_schema" must be present.')
             ->end()
         ;
         // @formatter:on
+        // host, user, password, database
         return $parametersNode;
     }
 }
--- tests/phpunit/ConfigDefinitionTest.php
+++ tests/phpunit/ConfigDefinitionTest.php
@@ -1,11 +1,160 @@
-<?php declare(strict_types=1);
+<?php
 
-namespace MyComponent\Tests;
+declare(strict_types=1);
 
-use MyComponent\ConfigDefinition;
+namespace Keboola\SnowflakeDwhManager\Tests;
 
+use Keboola\SnowflakeDwhManager\ConfigDefinition;
 use PHPUnit\Framework\TestCase;
+use Symfony\Component\Config\Definition\Exception\InvalidConfigurationException;
+use Symfony\Component\Config\Definition\Processor;
 
 class ConfigDefinitionTest extends TestCase
 {
+    /**
+     * @dataProvider provideValidConfigs
+     */
+    public function testValidConfigs(array $expected, array $configurationData): void
+    {
+        $definition = new ConfigDefinition();
+        $processor = new Processor();
+        $processedConfiguration = $processor->processConfiguration(
+            $definition,
+            [$configurationData]
+        );
+        $this->assertSame($expected, $processedConfiguration);
+    }
+
+    /**
+     * @return mixed[][]
+     */
+    public function provideValidConfigs(): array
+    {
+        return [
+            'user' => [
+                [
+                    'parameters' => [
+                        'master_host' => 'host',
+                        'master_user' => 'user',
+                        'master_password' => 'password',
+                        'master_database' => 'database',
+                        'warehouse' => 'warehouse',
+                        'user' => [
+                            'email' => 'test@example.com',
+                            'business_schemes' => [
+                                'dwh1',
+                                'dwh2',
+                            ],
+                            'disabled' => false,
+                        ],
+                    ],
+                ],
+                [
+                    'parameters' => [
+                        'master_host' => 'host',
+                        'master_user' => 'user',
+                        'master_password' => 'password',
+                        'master_database' => 'database',
+                        'warehouse' => 'warehouse',
+                        'user' => [
+                            'email' => 'test@example.com',
+                            'business_schemes' => [
+                                'dwh1',
+                                'dwh2',
+                            ],
+                        ],
+                    ],
+                ],
+            ],
+            'schema' => [
+                [
+                    'parameters' => [
+                        'master_host' => 'host',
+                        'master_user' => 'user',
+                        'master_password' => 'password',
+                        'master_database' => 'database',
+                        'warehouse' => 'warehouse',
+                        'business_schema' => [
+                            'schema_name' => 'dwh1',
+                        ],
+                    ],
+                ],
+                [
+                    'parameters' => [
+                        'master_host' => 'host',
+                        'master_user' => 'user',
+                        'master_password' => 'password',
+                        'master_database' => 'database',
+                        'warehouse' => 'warehouse',
+                        'business_schema' => [
+                            'schema_name' => 'dwh1',
+                        ],
+                    ],
+                ],
+            ],
+        ];
+    }
+
+    /**
+     * @dataProvider provideInvalidConfigs
+     */
+    public function testInvalidConfigs(
+        string $expectedException,
+        string $expectedExceptionMessage,
+        array $configurationData
+    ): void {
+        $definition = new ConfigDefinition();
+        $processor = new Processor();
+        $this->expectException($expectedException);
+        $this->expectExceptionMessage($expectedExceptionMessage);
+        $processedConfiguration = $processor->processConfiguration(
+            $definition,
+            [$configurationData]
+        );
+    }
+
+    /**
+     * @return mixed[][]
+     */
+    public function provideInvalidConfigs(): array
+    {
+        return [
+            'both user and schema' => [
+                InvalidConfigurationException::class,
+                'The "user" and "business_schema" options are mutually exclusive.',
+                [
+                    'parameters' => [
+                        'master_host' => 'host',
+                        'master_user' => 'user',
+                        'master_password' => 'password',
+                        'master_database' => 'database',
+                        'warehouse' => 'warehouse',
+                        'user' => [
+                            'email' => 'test@example.com',
+                            'business_schemes' => [
+                                'dwh1',
+                                'dwh2',
+                            ],
+                        ],
+                        'business_schema' => [
+                            'schema_name' => 'dwh1',
+                        ],
+                    ],
+                ],
+            ],
+            'neither user or schema' => [
+                InvalidConfigurationException::class,
+                'Either "user" or "business_schema" must be present.',
+                [
+                    'parameters' => [
+                        'master_host' => 'host',
+                        'master_user' => 'user',
+                        'master_password' => 'password',
+                        'master_database' => 'database',
+                        'warehouse' => 'warehouse',
+                    ],
+                ],
+            ],
+        ];
+    }
 }
