131a730fd68086656d8ee853d1636afeb7926abb
Jiri Semmler <jirka.semmler@gmail.com>
2025-10-26 01:15:32 +0200

    fix tests

--- tests/functional/DatadirScenarioTest.php
+++ tests/functional/DatadirScenarioTest.php
@@ -822,7 +822,7 @@ class DatadirScenarioTest extends AbstractDatadirTestCase
         );
         /** @var array<string, string> $value */
         $value = array_pop($filteredResult);
-
-        return $value['value'] === 'true';
+echo "password:" . $value;
+        return $value['value'] !== 'null';
     }
 }
